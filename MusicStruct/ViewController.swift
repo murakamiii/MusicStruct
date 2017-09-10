8a72cb0527d67dc161a11cf686503c4059b4c574
murakamiii <9khc727ru7@gmail.com>
2017-09-10 16:24:59 +0900

    Initial Commit

---
+++ MusicStruct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicStruct
+//
+//  Created by MURAKAMI on 2017/09/10.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
